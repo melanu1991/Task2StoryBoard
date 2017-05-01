0224312ab514460c38a29a8b5c84cf8698e93ed9
melanu <melanu001@mac-melanu.local>
2017-05-01 10:32:50 -0700

    Initial Commit

---
+++ Task2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task2
+//
+//  Created by melanu on 01.05.17.
+//  Copyright © 2017 melanu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task2
+//
+//  Created by melanu on 01.05.17.
+//  Copyright © 2017 melanu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
